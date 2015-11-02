ed9f231af99ad86fe94c77840d1c19d9791201d8
unknown <zhengjun1@zhengjun1.corp.ganji.com>
2015-11-02 19:07:22 +0800

    添加文章控制类、文章域类，文章视图类

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+   <head>
+     <meta charset="UTF-8">
+     <title>DevApp网站</title>
+   </head>
+   <body>
+      <div id="wrapper">
+       <nav>导航栏</nav>
+      
+       @yield('content')
+      </div>
+   </body>
+</html>

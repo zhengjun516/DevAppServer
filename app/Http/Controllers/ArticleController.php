94e2015a742b48a68fc78b953991ad5bd91a763e
unknown <zhengjun1@zhengjun1.corp.ganji.com>
2015-11-03 10:21:40 +0800

    无

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -17,7 +17,7 @@ class ArticleController extends Controller
     public function index()
     {
         //
-       /*  $articles = Article::all(); */
+       $articles = Article::all();
         //return view('article',compact('title','content'));
         return view('article');
     }

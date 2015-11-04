e5835c64f982d85a08e11a9d0caff79256cdebd9
unknown <zhengjun1@zhengjun1.corp.ganji.com>
2015-11-04 12:21:47 +0800

    去掉/index.php/路径

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,7 +1,7 @@
 @extends('app')
 @section('content')
   @foreach($articles as $article)
-      <h1><a href="/index.php/articles/{{ $article->id }}">{{$article->title}}</a></h1>
+      <h1><a href="/articles/{{ $article->id }}">{{$article->title}}</a></h1>
       <p>{{$article->intro}}</p>
       <hr>
   @endforeach
